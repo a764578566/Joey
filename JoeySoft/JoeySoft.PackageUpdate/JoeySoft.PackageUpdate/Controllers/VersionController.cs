0bc8f7de75aa11215f750a2d867c366423f8b78b
zhoujr <012wsxz@163.com>
2018-06-25 19:16:27 +0800

    添加更新服务(待开发)

---
+++ JoeySoft/JoeySoft.PackageUpdate/JoeySoft.PackageUpdate/Controllers/VersionController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JoeySoft.PackageUpdate.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoeySoft.PackageUpdate.Controllers
+{
+    /// <summary>
+    /// 版本Controller
+    /// </summary>
+    [Produces("application/json")]
+    [Route("api/Version")]
+    public class VersionController : Controller
+    {
+        private List<JoeySoftVersion> joeySoftVersions;
+        /// <summary>
+        /// 版本Controller初始化
+        /// </summary>
+        /// <param name="joeySoftVersions"></param>
+        public VersionController(List<JoeySoftVersion> joeySoftVersions)
+        {
+            this.joeySoftVersions = joeySoftVersions;
+        }
+
+        // GET: api/Version
+        /// <summary>
+        /// 获取所有软件信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<JoeySoftVersion> Get()
+        {
+            return this.joeySoftVersions;
+        }
+
+        // GET: api/Version/5
+        /// <summary>
+        /// 获取软件最新版本
+        /// </summary>
+        /// <param name="name">软件名称</param>
+        /// <returns></returns>
+        [HttpGet("{name}")]
+        public JoeySoftVersion Get(string name)
+        {
+            return this.joeySoftVersions.FirstOrDefault(n => n.JoeySoftName == name);
+        }
+
+        // POST: api/Version
+        /// <summary>
+        /// 添加软件版本
+        /// </summary>
+        /// <param name="value"></param>
+        [HttpPost]
+        public void Post([FromBody]JoeySoftVersion value)
+        {
+
+        }
+
+        // PUT: api/Version/5
+        /// <summary>
+        /// 修改软件版本信息
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
+        [HttpPut("{name}")]
+        public void Put(string name, [FromBody]JoeySoftVersion value)
+        {
+            var joeySoft = this.joeySoftVersions.FirstOrDefault(n => n.JoeySoftName == name);
+            if (joeySoft != null)
+            {
+                joeySoft.Version = value.Version;
+            }
+
+            //todo 保存到json中
+        }
+
+        // DELETE: api/ApiWithActions/5
+        /// <summary>
+        /// 删除软件版本
+        /// </summary>
+        /// <param name="name"></param>
+        [HttpDelete("{name}")]
+        public void Delete(string name)
+        {
+
+        }
+    }
+}
