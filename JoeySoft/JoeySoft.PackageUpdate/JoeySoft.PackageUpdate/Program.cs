628ecddd18443d95cf6f177d0ffdd97be729ca35
zhoujr <012wsxz@163.com>
2018-06-26 18:23:57 +0800

    修改地址

--- JoeySoft/JoeySoft.PackageUpdate/JoeySoft.PackageUpdate/Program.cs
+++ JoeySoft/JoeySoft.PackageUpdate/JoeySoft.PackageUpdate/Program.cs
@@ -20,6 +20,7 @@ namespace JoeySoft.PackageUpdate
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("localhost:80")
                 .Build();
     }
 }
