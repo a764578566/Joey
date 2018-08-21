1d6691604d84c1aad13efcd5c3fca2c553e672e6
zhoujr <012wsxz@163.com>
2018-08-21 09:37:05 +0800

    完成git 50%

--- JoeySoft/JoeySoft.TfsDevelop/Joey.Git/GitHelper.cs
+++ JoeySoft/JoeySoft.TfsDevelop/Joey.Git/GitHelper.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using JoeySoft.JoeyLog;
+using Octokit;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +14,70 @@ namespace Joey.Git
     /// </summary>
     public class GitHelper
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        public GitHelper()
+        {
+            RepositoryInformation github = RepositoryInformation.GetRepositoryInformationForPath("D:\\github\\microsoft\\core", gitPath);
+            string str = github.Pull();
+        }
 
+        /// <summary>
+        /// 获取git地址
+        /// </summary>
+        private string gitPath = System.IO.Path.Combine(EnvironmentVariable, "git.exe");
+
+        /// <summary>
+        /// 获取git地址
+        /// </summary>
+        private static string EnvironmentVariable
+        {
+            get
+            {
+                string sPath = System.Environment.GetEnvironmentVariable("Path");
+                var result = sPath.Split(';');
+                for (int i = 0; i < result.Length; i++)
+                {
+                    if (result[i].Contains(@"Git\cmd"))
+                    {
+                        sPath = result[i];
+                    }
+                }
+                return sPath;
+            }
+        }
+
+        /// <summary>
+        /// git pull
+        /// </summary>
+        public void Pull()
+        {
+            Logging.WriteLog(string.Format("环境路径：{0}", gitPath));
+            Process p = new Process();
+            p.StartInfo.FileName = gitPath;
+            p.StartInfo.Arguments = "git pull";
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.WorkingDirectory = "D:\\github\\microsoft\\core";
+            p.OutputDataReceived += OnOutputDataReceived;
+            p.Start();
+            p.BeginOutputReadLine();
+            p.WaitForExit();
+        }
+
+        /// <summary>
+        /// 获取返回数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnOutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e != null && !string.IsNullOrEmpty(e.Data))
+            {
+                Logging.WriteLog(e.Data);
+            }
+        }
     }
 }
---
+++ JoeySoft/JoeySoft.TfsDevelop/Joey.GitTest/Class1.cs
@@ -0,0 +1,22 @@
+﻿using Joey.Git;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Joey.GitTest
+{
+
+    public class GitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            GitHelper git = new GitHelper();
+            Assert.Empty("");
+        }
+
+    }
+}
