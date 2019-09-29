81c87190476a887fcb07ce0d04f6f147acacbc97
zhoujr01@mingyuanyun.com <764578566@163.com>
2019-09-29 10:14:19 +0800

    T4模板修改

--- JoeySoft/JoeySoft.MingYuan/JoeySoft.MingYuan.TestT4/JoeySoft.MingYuan.T4Test/DomainService/DomainServiceTestTemplate.cs
+++ JoeySoft/JoeySoft.MingYuan/JoeySoft.MingYuan.TestT4/JoeySoft.MingYuan.T4Test/DomainService/DomainServiceTestTemplate.cs
@@ -1,4 +1,4 @@
-using Mysoft.Clgyl.ProductMng.DomainServices;
+﻿using Mysoft.Clgyl.ProductMng.DomainServices;
 using Mysoft.Clgyl.ProductMng.DomainServices.Fakes;
 using Mysoft.Clgyl.ProductMng.Model;
 using Mysoft.Clgyl.Utility;
@@ -19,35 +19,35 @@ using System.Threading.Tasks;
 namespace Mysoft.Clgyl.ProductMng.UnitTest.DomainServices
 {
 	/// <summary>
-    /// 材料安装费领域服务单测
+    /// 材料运输费领域服务单测
     /// </summary>
     [TestFixture]
-    public class ProductInstallFeeDomainServiceTest
+    public class ProductTransportCostDomainServiceTest
     {
-		private readonly ProductInstallFeeDomainService _productInstallFeeDomainService = new ProductInstallFeeDomainService();
+		private readonly ProductTransportCostDomainService _productTransportCostDomainService = new ProductTransportCostDomainService();
 
-        private readonly EntityMocker<ProductInstallFee> _productInstallFeeMock = new EntityMocker<ProductInstallFee>();
+        private readonly EntityMocker<ProductTransportCost> _productTransportCostMock = new EntityMocker<ProductTransportCost>();
         
         private readonly StubFormEntityDomainService _formEntity = new StubFormEntityDomainService();
         #region 接口服务
         #endregion
 
-        private readonly string _productInstallFeeFileJsonName = "ProductInstallFee.json";
+        private readonly string _productTransportCostFileJsonName = "ProductTransportCost.json";
 
         [SetUp]
         public void InitResource()
         {
             TestMockServiceResolver.RegisterInstance<FormEntityDomainService, StubFormEntityDomainService>(_formEntity);
-            _productInstallFeeMock.LoadData(_productInstallFeeFileJsonName);
+            _productTransportCostMock.LoadData(_productTransportCostFileJsonName);
         }
 
-        #region  材料安装费领域服务单测 2个方法
-        [Test(Description = "根据材料GUIDs，获取安装费")]
+        #region  材料运输费领域服务单测 2个方法
+        [Test(Description = "根据材料GUIDs，获取运费")]
         public void FindByProductGUIDs_Test()
         {
             
         }
-        [Test(Description = "根据材料GUID，获取安装费")]
+        [Test(Description = "根据材料GUID，获取运费")]
         public void FindByProductGUID_Test()
         {
             
